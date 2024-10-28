d6daa2ffe9512e6a6b829f89a860e6911684eeaa
Weegoos <batyr.ashim05@mail.ru>
2024-10-28 17:29:18 +0500

    IARS Project: Detailed information has been configured

--- src/components/CardPage/DetailedInformation.vue
+++ src/components/CardPage/DetailedInformation.vue
@@ -3,10 +3,7 @@
     <q-dialog v-model="confirm" persistent>
       <q-card>
         <q-card-section class="row items-center">
-          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
-          <span class="q-ml-sm"
-            >You are currently not connected to any network.</span
-          >
+          <span>Подробная информация будет здесь</span>
         </q-card-section>
         <q-card-actions align="right">
           <q-btn flat label="Закрыть" color="primary" @click="closeWindow" />
