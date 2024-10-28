79bffe32970636dd8f479516d57020ff85b0c257
Weegoos <batyr.ashim05@mail.ru>
2024-10-28 16:37:48 +0500

    IARS Project: Project has been configured

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <q-card class="my-card">
-      <section class="row q-gutter-md">
-        <div class="col">
+    <q-card class="">
+      <section class="row q-gutter-md" style="align-items: stretch">
+        <div class="col-4">
           <q-card-section>
             <span class="infoHeadline">№ (порядковый)</span>
             <p class="infoStyle text-capitalize"></p>
@@ -10,7 +10,42 @@
             <p class="infoStyle text-capitalize"></p>
             <span class="infoHeadline">Дата создания документа</span>
             <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">ИИН вызываемого</span>
+            <p class="infoStyle text-capitalize"></p>
           </q-card-section>
+        </div>
+        <div class="col-3">
+          <q-card-section>
+            <span class="infoHeadline">ФИО вызываемого</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Номер УД</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Статья УК</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Время прихода</span>
+            <p class="infoStyle text-capitalize"></p>
+          </q-card-section>
+        </div>
+
+        <div class="col-3">
+          <q-card-section>
+            <span class="infoHeadline">Место работы</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Должность</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Регион</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">Относится ли к бизнесу</span>
+            <p class="infoStyle text-capitalize"></p>
+            <span class="infoHeadline">ФИО согласующего</span>
+            <p class="infoStyle text-capitalize"></p>
+          </q-card-section>
+        </div>
+        <div class="col">
+          <q-card-actions vertical align="center">
+            <q-btn flat label="Подробнее" />
+            <q-btn flat label="Согласовать" />
+          </q-card-actions>
         </div>
       </section>
     </q-card>
