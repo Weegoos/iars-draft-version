540d87705eed5ea4b5a6bf41b0ee6f88cd0633db
Weegoos <batyr.ashim05@mail.ru>
2024-11-06 19:16:25 +0500

    Aitu project challenge 20: Create Card has been updated

--- src/pages/CreateCardPage.vue
+++ src/pages/CreateCardPage.vue
@@ -2,17 +2,110 @@
   <div class="q-px-xl q-py-md">
     <q-card class="my-card">
       <q-card-section>
-        <div class="text-h6">Здесь будем создавать карточки</div>
+        <div class="row q-gutter-sm">
+          <div class="col">
+            <q-input v-model="idNumber" type="text" label="Номер УД" />
+          </div>
+          <div class="col">
+            <q-input v-model="iin" type="text" label="ИИН вызываемого" />
+          </div>
+          <div class="col">
+            <q-input v-model="binAndIin" type="text" label="БИН/ИИН" />
+          </div>
+        </div>
       </q-card-section>
       <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <div class="row q-gutter-sm">
+          <div class="col">
+            <q-input
+              v-model="positionTheCalledPerson"
+              type="text"
+              label="Должность вызываемого"
+            />
+          </div>
+          <div class="col">
+            <q-input v-model="region" type="text" label="Регион" />
+          </div>
+          <div class="col">
+            <q-input
+              v-model="plannedInvestigateActions"
+              type="text"
+              label="Планируемые следственные действия"
+            />
+          </div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-gutter-sm">
+          <div class="col">
+            <q-input v-model="date">
+              <template v-slot:prepend>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+          <div class="col">
+            <q-input
+              v-model="locationOfTheEvent"
+              type="text"
+              label="Место проведения"
+            />
+          </div>
+        </div>
       </q-card-section>
     </q-card>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const idNumber = ref("");
+const iin = ref("");
+const binAndIin = ref("");
+const positionTheCalledPerson = ref("");
+const region = ref("");
+const plannedInvestigateActions = ref("");
+const date = ref("Дата и время проведения");
+const locationOfTheEvent = ref("");
 </script>
 
 <style></style>
