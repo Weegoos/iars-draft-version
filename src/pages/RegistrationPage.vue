aaa667167047812b000e1bfab2cdf6dfd4deff7a
Weegoos <batyr.ashim05@mail.ru>
2024-11-11 11:19:56 +0500

    Aitu Project Challenge: Registration page has been updated

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -3,11 +3,38 @@
     <q-card class="my-card" style="width: 70vw">
       <q-card-section>
         <div class="row q-gutter-sm">
-          <div class="col"></div>
+          <div class="col">
+            <q-input
+              v-model="name"
+              type="text"
+              label="Напишите имя"
+              hint="Например: Айсултан"
+            />
+          </div>
+          <div class="col">
+            <q-input
+              v-model="secondName"
+              type="text"
+              label="Напишите фамилию"
+              hint="Например: Хаббасов"
+            />
+          </div>
         </div>
       </q-card-section>
       <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <div class="row q-gutter-sm">
+          <div class="col">
+            <q-input
+              v-model="email"
+              type="text"
+              label="Напишите почту"
+              hint="Например: example@gmail.com"
+            />
+          </div>
+          <div class="col">
+            <q-input v-model="email" type="text" label="Напишите почту" />
+          </div>
+        </div>
       </q-card-section>
     </q-card>
   </div>
