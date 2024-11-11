1c6f46af45b0e1ea02adbd9b417716d7131536dc
Weegoos <batyr.ashim05@mail.ru>
2024-11-11 11:33:02 +0500

    Aitu project challenge: Registration page has been updated

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -26,22 +26,56 @@
           <div class="col">
             <q-input
               v-model="email"
-              type="text"
+              type="email"
               label="Напишите почту"
               hint="Например: example@gmail.com"
             />
           </div>
           <div class="col">
-            <q-input v-model="email" type="text" label="Напишите почту" />
+            <q-input
+              v-model="password"
+              type="password"
+              label="Напишите пароль"
+              hint="Не менее 6 символов"
+            />
           </div>
         </div>
       </q-card-section>
+      <q-card-section>
+        <div class="row q-gutter-sm">
+          <div class="col">
+            <q-input
+              v-model="department"
+              type="text"
+              label="Напишите департамент"
+            />
+          </div>
+          <div class="col">
+            <q-input v-model="iin" type="text" label="Напишите ИИН" />
+          </div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <q-input v-model="region" type="text" label="Напишите регион" />
+      </q-card-section>
+      <q-card-actions align="center">
+        <q-btn
+          color="positive"
+          no-caps
+          label="Зарегистрироваться"
+          @click="registration"
+        />
+        <q-btn color="primary" no-caps label="Есть аккаунт?" @click="onClick" />
+      </q-card-actions>
     </q-card>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+const registration = async () => {};
 </script>
 
 <style></style>
