ffe3250f33f87d67decaf4f9af02bb4e5045cf9c
Weegoos <batyr.ashim05@mail.ru>
2024-11-19 18:25:04 +0500

    Aitu project challenge: GetAllConclusion method has been added

--- src/components/Admin/AdminConclusion.vue
+++ src/components/Admin/AdminConclusion.vue
@@ -2,6 +2,34 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { getCurrentInstance } from "vue";
+const { proxy } = getCurrentInstance();
+const serverUrl = proxy.$serverUrl;
+
+const allConclusion = ref("");
+const getAllConclusion = async () => {
+  try {
+    const response = await axios.get(`${serverUrl}admin/allConclusions`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      withCredentials: true,
+    });
+    console.log(response.data);
+    allConclusion.value = response.data;
+  } catch (error) {
+    console.error("Ошибка при получении всех документов:", error);
+    throw error;
+  }
+};
+
+onMounted(() => {
+  getAllConclusion();
+});
+</script>
 
 <style></style>
