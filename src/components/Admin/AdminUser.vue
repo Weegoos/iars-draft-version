4798ba18ccd708c1944b4847d9f184feacf709f1
Weegoos <batyr.ashim05@mail.ru>
2024-11-19 19:34:16 +0500

    Aitu project challenge has been updated

--- src/components/Admin/AdminUser.vue
+++ src/components/Admin/AdminUser.vue
@@ -3,12 +3,63 @@
     <section v-for="(items, index) in allusers" :key="index" class="q-mt-md">
       <q-card class="my-card">
         <q-card-section>
-          <div class="text-h6">Our Changing Planet</div>
-          <div class="text-subtitle2">by John Doe</div>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <span class="infoHeadline">ФИО</span>
+              <p class="infoStyle">{{ items.name }} {{ items.secondName }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">Почта</span>
+              <p class="infoStyle">{{ items.email }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">ИИН</span>
+              <p class="infoStyle">{{ items.iin }}</p>
+            </div>
+          </div>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <span class="infoHeadline">Дата регистрации</span>
+              <p class="infoStyle">{{ items.registrationDate }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">Департамент</span>
+              <p class="infoStyle">{{ items.department.name }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">Регион</span>
+              <p class="infoStyle">{{ items.department.region }}</p>
+            </div>
+          </div>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <span class="infoHeadline">Должность</span>
+              <p class="infoStyle">{{ items.jobTitle.name }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">Количество документов</span>
+              <p class="infoStyle">{{ items.conclusions.length }}</p>
+            </div>
+            <div class="col">
+              <span class="infoHeadline">Полученные документы</span>
+              <p class="infoStyle">{{ items.receivedConclusionDtos.length }}</p>
+            </div>
+          </div>
         </q-card-section>
-        <q-card-section>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn
+            color="blue-13"
+            no-caps
+            label="Повышение"
+            @click="promoteUser(items)"
+          />
+          <q-btn
+            color="negative"
+            no-caps
+            label="Удалить пользователя"
+            @click="onClick"
+          />
+        </q-card-actions>
       </q-card>
     </section>
   </div>
@@ -51,6 +102,37 @@ const getAllUsers = async () => {
 onMounted(() => {
   getAllUsers();
 });
+
+const promoteUser = async (item) => {
+  const accessToken = localStorage.getItem("accessToken");
+  try {
+    const response = await axios.patch(
+      `${serverUrl}admin/promote?IIN=${item.iin}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        withCredentials: true,
+      }
+    );
+    console.log("IIN:", item.iin);
+    console.log("Ответ сервера:", response.data);
+
+    $q.notify({
+      message: `Пользователь ${item.name} ${item.secondName} успешно повышен в должности`,
+      icon: "check",
+      color: "positive",
+    });
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
+  } catch (error) {
+    console.error("Ошибка при повышении:", error.response || error);
+  }
+};
 </script>
 
 <style></style>
