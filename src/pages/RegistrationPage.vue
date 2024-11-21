c079476da950b5a9ff37aee7ec8fa76785bfc7ae
Weegoos <batyr.ashim05@mail.ru>
2024-11-21 12:29:45 +0500

    Aitu project challenge: Registration page has been updated

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -1,86 +1,153 @@
 <template>
-  <div class="fixed-center">
-    <q-card class="my-card" style="width: 50vw">
-      <q-card-section align="center">
-        <p class="text-h5">Регистрация</p>
-      </q-card-section>
-      <q-card-section>
-        <div class="row q-gutter-sm">
-          <div class="col">
-            <q-input
-              v-model="name"
-              type="text"
-              label="Напишите имя"
-              hint="Например: Айсултан"
-            />
+  <div class="fixed-center" style="display: flex; height: 100vh; width: 100vw">
+    <!-- Левая половина с видео -->
+    <div style="flex: 1; position: relative; overflow: hidden">
+      <video
+        src="../assets/video/background.mp4"
+        autoplay
+        muted
+        loop
+        playsinline
+        style="width: 100%; height: 100%; object-fit: cover"
+      ></video>
+
+      <!-- Надпись -->
+      <div style="position: absolute; color: red; top: 45px">
+        <q-carousel
+          v-model="slide"
+          transition-prev="scale"
+          transition-next="scale"
+          swipeable
+          animated
+          control-color="white"
+          navigation
+          padding
+          arrows
+          height="300px"
+          class="bg-primary text-white shadow-1 rounded-borders"
+        >
+          <q-carousel-slide name="style" class="column no-wrap flex-center">
+            <q-icon name="style" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="tv" class="column no-wrap flex-center">
+            <q-icon name="live_tv" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="layers" class="column no-wrap flex-center">
+            <q-icon name="layers" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="map" class="column no-wrap flex-center">
+            <q-icon name="terrain" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+    </div>
+
+    <!-- Правая половина с текстом -->
+    <div
+      style="
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        background-color: rgba(255, 255, 255, 0.8);
+      "
+    >
+      <q-card class="my-card" style="width: 90%">
+        <q-card-section align="center">
+          <p class="text-h5">Регистрация</p>
+        </q-card-section>
+        <q-card-section>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <q-input
+                v-model="name"
+                type="text"
+                label="Напишите имя"
+                hint="Например: Айсултан"
+              />
+            </div>
+            <div class="col">
+              <q-input
+                v-model="secondName"
+                type="text"
+                label="Напишите фамилию"
+                hint="Например: Хаббасов"
+              />
+            </div>
           </div>
-          <div class="col">
-            <q-input
-              v-model="secondName"
-              type="text"
-              label="Напишите фамилию"
-              hint="Например: Хаббасов"
-            />
+        </q-card-section>
+        <q-card-section>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <q-input
+                v-model="email"
+                type="email"
+                label="Напишите почту"
+                hint="Например: example@gmail.com"
+              />
+            </div>
+            <div class="col">
+              <q-input
+                v-model="password"
+                type="password"
+                label="Напишите пароль"
+                hint="Не менее 6 символов"
+              />
+            </div>
           </div>
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <div class="row q-gutter-sm">
-          <div class="col">
-            <q-input
-              v-model="email"
-              type="email"
-              label="Напишите почту"
-              hint="Например: example@gmail.com"
-            />
+        </q-card-section>
+        <q-card-section>
+          <div class="row q-gutter-sm">
+            <div class="col">
+              <q-select
+                v-model="department"
+                :options="departmentList"
+                label="Введите департамент"
+              />
+            </div>
+            <div class="col">
+              <q-input v-model="iin" type="number" label="Напишите ИИН" />
+            </div>
           </div>
-          <div class="col">
-            <q-input
-              v-model="password"
-              type="password"
-              label="Напишите пароль"
-              hint="Не менее 6 символов"
-            />
-          </div>
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <div class="row q-gutter-sm">
-          <div class="col">
-            <q-select
-              v-model="department"
-              :options="departmentList"
-              label="Введите департамент"
-            />
-          </div>
-          <div class="col">
-            <q-input v-model="iin" type="number" label="Напишите ИИН" />
-          </div>
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <q-select
-          v-model="region"
-          :options="regionList"
-          v-show="isRegionVisible"
-          label="Введите регион"
-        />
-      </q-card-section>
-      <q-card-actions align="center">
-        <q-btn
-          color="positive"
-          no-caps
-          label="Зарегистрироваться"
-          @click="registration"
-        />
-        <q-btn
-          color="primary"
-          no-caps
-          label="Есть аккаунт?"
-          @click="pushToAuthorization"
-        />
-      </q-card-actions>
-    </q-card>
+        </q-card-section>
+        <q-card-section>
+          <q-select
+            v-model="region"
+            :options="regionList"
+            v-show="isRegionVisible"
+            label="Введите регион"
+          />
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn
+            color="positive"
+            no-caps
+            label="Зарегистрироваться"
+            @click="registration"
+          />
+          <q-btn
+            color="primary"
+            no-caps
+            label="Есть аккаунт?"
+            @click="pushToAuthorization"
+          />
+        </q-card-actions>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -89,6 +156,8 @@ import axios from "axios";
 import { useQuasar } from "quasar";
 import { onMounted, ref, watch } from "vue";
 import { useRouter } from "vue-router";
+
+const slide = ref("style");
 
 const name = ref("");
 const secondName = ref("");
@@ -183,4 +252,4 @@ onMounted(() => {
 });
 </script>
 
-<style></style>
+<style scoped></style>
